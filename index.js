ebdcf3a00248db03aa60bd4b44fc761b5f74917f
Kaustav Majumder <kmajumder2002@gmail.com>
2023-08-21 03:09:36 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// header
+const navlinks=document.querySelector('.portfolio-header-tags');
+const menuicon=document.getElementById("menu-icon");
+
+menuicon.addEventListener("click",()=>{
+    menuicon.classList.toggle('close');
+    navlinks.classList.toggle('open');
+})
+// 
+const arrow=document.querySelector('.arrow');
+window.onscroll=()=>{
+    if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+        arrow.style="position:fixed";
+    }
+    else{
+        arrow.style="display:none";
+    }
+}
+// Typing effect
+const words=["Web Developer","C++ Programmer","Student"];
+const dynamicText=document.querySelector(".intro-changing-text");
+let wordIndex=0;
+let charIndex=0;
+let isDeleting=false;
+
+const typeEffect=()=>{
+    const currentWord=words[wordIndex];
+    const currentChar=currentWord.substring(0,charIndex);
+    dynamicText.textContent=currentChar;
+
+    if(!isDeleting && charIndex<currentWord.length){
+        charIndex++;
+        setTimeout(typeEffect,200);
+    }
+    else if(isDeleting && charIndex>0){
+        charIndex--;
+        setTimeout(typeEffect,100);
+    }
+    else{
+        isDeleting=!isDeleting;
+        wordIndex=!isDeleting?(wordIndex+1)%words.length:wordIndex;
+        setTimeout(typeEffect,1200);
+    }
+    
+}
+typeEffect();
+
+const observer=new IntersectionObserver((entires)=>{
+    entires.forEach((entry)=> {
+    
+        entry.target.classList.toggle("show",entry.intersectionRatio)
+    });
+})
+
+const hiddenElements=document.querySelectorAll(".hidden");
+hiddenElements.forEach((el)=>observer.observe(el));
